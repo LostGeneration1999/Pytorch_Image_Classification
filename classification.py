fafd73d65b294dee32c2a07a183e5a429c765ff0
masaya <masaya4426saganosen@gmail.com>
2020-07-11 00:42:27 +0900

    classification test

--- classification.py
+++ classification.py
@@ -1,5 +1,5 @@
 import io
-
+import json
 import torchvision.transforms as transforms
 from torchvision import models
 from PIL import Image
@@ -7,6 +7,10 @@ from PIL import Image
 mean = [0.485, 0.456, 0.406]
 std = [0.229, 0.224, 0.225]
 size = 224
+
+model = models.densenet121(pretrained=True)
+model.eval()
+class_index = json.load(open('./imagenet_class_index.json'))
 
 def transform_image(image_bytes, size, mean, std):
     my_transforms = transforms.Compose(
@@ -18,10 +22,14 @@ def transform_image(image_bytes, size, mean, std):
     image = Image.open(io.BytesIO(image_bytes))
     return my_transforms(image).unsqueeze(0)
 
-with open('./fukuoka.jpg', 'rb') as f:
-    image_bytes = f.read()
+def get_prediction(image_bytes, size, mean, std):
     tensor = transform_image(image_bytes, size, mean, std)
-    print(tensor)
-
-
+    outputs = model.forward(tensor)
+    _, pred = outputs.max(1)
+    prediction_class = str(pred.item())
+    return class_index[prediction_class]
     
+with open('./dog.jpg', 'rb') as f:
+    image_bytes = f.read()
+    print(get_prediction(image_bytes, size, mean, std))
+
